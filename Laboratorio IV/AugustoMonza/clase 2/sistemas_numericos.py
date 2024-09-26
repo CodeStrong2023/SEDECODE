16d0a36dc026224d8881e73458ce113ab980639c
AugustoMonza <augustomonza82@gmail.com>
2024-09-26 19:17:11 -0300

    FEAT: se agregaron las clases de laboratorio

---
+++ Laboratorio IV/AugustoMonza/clase 2/sistemas_numericos.py
@@ -0,0 +1,44 @@
+# sistema decimal
+a = 10
+print(a)
+
+# sistema binario
+a = 0b1010
+print(a)
+
+# sistema octal
+a = 0o12
+print(a)
+
+# sistema hexadecimal
+a = 0xa
+print(a)
+
+# --------------------------------------------------------------------------------------------------------------
+
+# base decimal
+a = int('23', 10)
+print(a)
+
+# base binario
+a = int('1010', 2)
+print(a)
+
+# base octal
+a = int('12', 8)
+print(a)
+
+# base hexadecimal
+a = int('A', 16)
+print(a)
+
+# --------------------------------------------------------------------------------------------------
+import math
+infinito_positivo = float('inf')
+print(infinito_positivo)
+print(math.isinf(infinito_positivo))
+
+infinito_positivo = float('-inf')
+print(infinito_positivo)
+print(math.isinf(infinito_positivo))
+
