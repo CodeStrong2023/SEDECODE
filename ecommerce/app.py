c7fe34f2feb24a90292c4606e016f4463980d957
AugustoMonza <augustomonza82@gmail.com>
2024-09-25 21:47:51 -0300

    FEAT: Added loggin

--- ecommerce/app.py
+++ ecommerce/app.py
@@ -14,5 +14,13 @@ def about():
 def contact_us():
     return render_template('website/contact_us.html')
 
+@app.route('/admin')
+def admin_index():
+    return render_template('admin/index.html')
+
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin/login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
