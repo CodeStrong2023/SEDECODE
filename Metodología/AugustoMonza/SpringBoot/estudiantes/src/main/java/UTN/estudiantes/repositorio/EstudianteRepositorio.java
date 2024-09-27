bded43b832f956bdef1b901b8406e5b08ab44ab6
AugustoMonza <augustomonza82@gmail.com>
2024-09-27 17:39:37 -0300

    FEAT: se agregaron los proyectos correspondientes a java

---
+++ Metodología/AugustoMonza/SpringBoot/estudiantes/src/main/java/UTN/estudiantes/repositorio/EstudianteRepositorio.java
@@ -0,0 +1,8 @@
+package UTN.estudiantes.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.estudiantes.modelo.Estudiante;
+
+
+public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
+}
