0884c59dbdb42dfa282d72e127adf674fb415552
Gabriel Hernan Osores <96618992+Gabo1618@users.noreply.github.com>
2024-10-16 10:26:45 -0300

    Update logger_base.py

--- Laboratorio IV/Gabriel Osores/logger_base.py
+++ Laboratorio IV/Gabriel Osores/logger_base.py
@@ -7,3 +7,11 @@ log.basicConfig(level=log.INFO,
                     log.FileHandler('logs/logfile.log', encoding='utf-8'),
                     log.StreamHandler()
                 ])
+
+
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
