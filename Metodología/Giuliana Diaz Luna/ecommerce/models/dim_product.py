94b9c4148221592da8c285deaa081c0646983d4c
Giuliana <giulygael2018@gmail.com>
2024-10-27 12:18:46 -0300

    Se modifica formulario de productos y se mejora el diseño visual del ecommerce

---
+++ Metodología/Giuliana Diaz Luna/ecommerce/models/dim_product.py
@@ -0,0 +1,18 @@
+from base_model import DatabaseTableBuilder
+from sqlalchemy import Column, Integer, Text, Float, DateTime, func
+
+base = DatabaseTableBuilder.get_instance()
+
+
+class DimProduct(base):
+    """
+    A class representing the dimension table 'dim_product' in the database.
+    """
+    __tablename__ = 'dim_product'
+    sk_product = Column(Integer, primary_key=True, autoincrement=True)
+    product_name = Column(Text, nullable=False)
+    product_price = Column(Float, nullable=False)
+    product_stock = Column(Integer, nullable=False)
+    product_url = Column(Text, nullable=False)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime, onupdate=func.now(), nullable=False)
---
+++ Metodología/Giuliana Diaz Luna/ecommerce/static/js/login.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("login.js está funcionando correctamente");
+    const form = document.querySelector('form');
+
+    const apiSession = sessionStorage.getItem('api-session');
+
+    if (apiSession === 'Acceso autorizado') {
+        // Evita redirigir si ya estás en la página de inicio
+        if (window.location.pathname !== '/') {
+            window.location.href = '/';
+        }
+    } else if (!apiSession) {
+        // Evita redirigir si ya estás en la página de inicio de sesión
+        if (window.location.pathname !== '/admin/login') {
+            window.location.href = '/admin/login';
+        }
+    }
+
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault(); // Prevenir el envío predeterminado del formulario
+
+        const emailInput = document.getElementById('exampleInputEmail1');
+        const passwordInput = document.getElementById('exampleInputPassword1');
+
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        // Validación simple
+        if (!validateEmail(email)) {
+            alert('Please enter a valid email address.');
+            return;
+        }
+
+        if (password === "") {
+            alert('Please enter a password.');
+            return;
+        }
+
+        // Datos a enviar
+        const data = {
+            client_mail: email,
+            client_password: password
+        };
+
+        try {
+            const response = await fetch('/admin/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const responseData = await response.json();
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `¡${responseData.message}!`,
+                showConfirmButton: false,
+                timer: 1500
+            });
+            window.location.href = '/admin/products';
+            sessionStorage.setItem('api-session', responseData.message);
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Por favor, verifica tus credenciales y vuelve a intentarlo.');
+        }
+    });
+
+    // Función para validar correos electrónicos
+    function validateEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+});
