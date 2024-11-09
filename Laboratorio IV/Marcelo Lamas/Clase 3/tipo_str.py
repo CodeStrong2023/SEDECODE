ab2b2a5303b1f925043fd6377af6d5e3b52898ad
Marcelo <marcelolamas2495@gmail.com>
2024-11-09 15:24:23 -0300

    FEAT : Clase Python

---
+++ Laboratorio IV/Marcelo Lamas/Clase 3/tipo_str.py
@@ -0,0 +1,7 @@
+variable = 'Adios'
+mensaje = 'Hola' 'Alumnos' + variable
+mensaje += ', Terminamos'
+print(mensaje)
+
+
+help(str)
---
+++ Laboratorio IV/Marcelo Lamas/Clase 5/mi_clase.py
@@ -0,0 +1,13 @@
+class MiClase: 
+
+    def __init__(self):
+        '''
+        Aqui esta el metodo dunder init
+        '''
+    def mi_metodo(self,param1, param2):        
+        '''
+        :param 1, 2 , ....
+        :return
+        '''
+
+    
