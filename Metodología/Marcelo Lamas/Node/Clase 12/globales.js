bf90fe7d1d07ed21fc5ffa291344fa9f75efd466
Marcelo <marcelolamas2495@gmail.com>
2024-11-09 14:09:30 -0300

    FEAT : Clase Node

---
+++ Metodología/Marcelo Lamas/Node/Clase 12/globales.js
@@ -0,0 +1,34 @@
+console.log();
+
+console.error();
+
+setTimeout(()=>{});
+
+setInterval(()=>{});
+
+setImmdiate(()=>{});
+
+console.log(setInterval);
+
+Let i = 0;
+
+Let intervalo = setInterval(() =>{
+    console.log('Hola');
+    if (if == 3) {
+        clearInterval(intervalo);
+    }
+    i++;
+}, 1000);
+
+setImmdiate(()=>{
+    console.log('Saludo inmediato');
+})
+requiere();
+
+console.log(process);
+
+console.log(__filename);
+
+globalThis.miVariable = 'mi variable global';
+console.log(miVariable);
+
